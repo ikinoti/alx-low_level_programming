6fe710b1a03d80284811b0a3f6d1f1cafc89cd53
ikinoti <gitongakinoti1@gmail.com>
2022-03-15 14:16:58 -0700

    modified

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,23 +13,29 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar(n + '0');
-				if (n == 98)
-					continue;
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else if (n == 98)
+			{
+				printf("%d", n);
+			}
 			n++;
 		}
 	}
-	else
+	else if (n >= 98)
 	{
 		while (n >= 98)
 		{
-			_putchar(n + '0');
-			if (n == 98)
-				continue;
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else if (n == 98)
+			{
+				printf("%d", n);
+			}
 			n--;
 		}
 	}
