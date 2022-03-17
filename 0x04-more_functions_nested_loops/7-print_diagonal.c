07f82fc5c0abb5e100281446a3d103081b44e596
ikinoti <gitongakinoti1@gmail.com>
2022-03-17 12:03:06 -0700

    h

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,13 +17,13 @@ void print_diagonal(int n)
 		{
 			int j = 0;
 
-			while (j < i)
+			while (j <= i)
 			{
 				_putchar(' ');
 				j++;
 			}
 
-			_putchar('\');
+			_putchar('\\');
 			_putchar('\n');
 			i++;
 		}
