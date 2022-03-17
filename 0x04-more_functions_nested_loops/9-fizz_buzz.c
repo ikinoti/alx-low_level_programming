1c9579daa73b84c6f1d6daad949429b536417cc6
ikinoti <gitongakinoti1@gmail.com>
2022-03-17 12:42:26 -0700

    :modified :

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,18 +15,30 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
 		else if (x % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else if (x % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else
+		{
 			printf("%i", x);
+		}
 
 		if (x < 100)
+		{
 			printf(" ");
+		}
 		else
+		{
 			print("\n");
+		}
 	}
 	return (0);
 }
