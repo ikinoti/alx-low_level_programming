21d6c3de1fb945b7fe3a0bb66ad68da448d9f65d
ikinoti <gitongakinoti1@gmail.com>
2022-04-01 04:39:06 -0700

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point of a function
+ *
+ * @argc: count of passed argument
+ * @argv: array of argument passed
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+
+	return (0);
+}
