5fc353355e485316a7461ab8ffd5cb6879c58d68
ikinoti <gitongakinoti1@gmail.com>
2022-04-14 08:12:15 -0700

    modified

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,7 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 	double sum = 0;
 
 	if (n == 0)
+	{
 		return (0);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
